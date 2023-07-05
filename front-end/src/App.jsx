6974a102963f4736cbf3ea5bbc306f5d5678fd09
Dawahreh <HAMZA.MOHMMAD99@YAHOO.COM>
2023-07-05 13:36:46 +0300

    first push

---
+++ front-end/src/App.jsx
@@ -0,0 +1,47 @@
+import HomePage from "./Pages/HomePage";
+import Navbar from "./Components/Navbar";
+import Footer from "./Components/Footer";
+import NotFound from "./Pages/404";
+import AboutUs from "./Pages/AboutUs";
+import ContactUs from "./Pages/ContactUs";
+import Blog from "./Pages/Blogs";
+import ProfilePage from "./Pages/ProfilePage";
+import Login from "./Pages/Login";
+import SignUp from "./Pages/Signup";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import { useEffect } from "react";
+
+function App() {
+  const ScrollToTop = () => {
+    const { pathname } = useLocation();
+
+    useEffect(() => {
+      window.scrollTo(0, 0);
+    }, [pathname]);
+
+    return null;
+  };
+
+  return (
+    <>
+      <BrowserRouter>
+        <ScrollToTop />
+
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/Blog" element={<Blog />} />
+          <Route path="/Login" element={<Login />} />
+          <Route path="/SignUp" element={<SignUp />} />
+          <Route path="/ProfilePage" element={<ProfilePage />} />
+          <Route path="/ContactUs" element={<ContactUs />} />
+          <Route path="/AboutUs" element={<AboutUs />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
