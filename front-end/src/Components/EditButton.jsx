02182edd29875f1f072ba0d3fce59b4072630687
Dawahreh <HAMZA.MOHMMAD99@YAHOO.COM>
2023-07-09 09:59:06 +0300

    fix total likes

--- front-end/src/Components/EditButton.jsx
+++ front-end/src/Components/EditButton.jsx
@@ -1,14 +1,17 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
-import { useEffect } from "react";
+import axios from "axios";
+
 const EditButton = ({ onEdit }) => {
   const [showForm, setShowForm] = useState(false);
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
+  const [userId, setUserId] = useState(null);
+  const [userData, setUserData] = useState(null);
 
   const handleEditClick = () => {
     setShowForm(true);
-    setName(""); // Set initial values for name and email
+    setName("");
     setEmail("");
   };
 
@@ -26,15 +29,10 @@ const EditButton = ({ onEdit }) => {
           },
         });
         setUserId(response.data.user.id);
-        console.log(response.data.user.email);
         let id = response.data.user.id;
         try {
           const response = await axios.get(
             `http://localhost:5000/api/users/${id}`
-          );
-          console.log(response.data);
-          console.log(
-            "tttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttt"
           );
           setUserData(response.data[0]);
           setName(response.data[0].firstName);
@@ -47,8 +45,6 @@ const EditButton = ({ onEdit }) => {
       console.error(error);
       localStorage.removeItem("auth");
       window.location.href = "http://localhost:3000/Login";
-    } finally {
-      console.log(false);
     }
   };
 
@@ -68,7 +64,6 @@ const EditButton = ({ onEdit }) => {
       })
       .then(function (response) {
         console.log(response);
-        // navigate("/ProfilePage")
         window.location.href = "http://localhost:3000/ProfilePage";
       })
       .catch(function (error) {
@@ -78,54 +73,66 @@ const EditButton = ({ onEdit }) => {
 
   if (showForm) {
     return ReactDOM.createPortal(
-      <div className="edit-form">
-        <form onSubmit={handleSubmit} className="w-full max-w-sm">
-          <div className="mb-4">
-            <label htmlFor="name" className="block text-gray-700">
-              Name:
-            </label>
-            <input
-              id="name"
-              type="text"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-              className="mt-1 block w-full px-4 py-2 rounded-md border-gray-300 focus:outline-none focus:ring-blue-500 focus:border-blue-500"
-            />
-          </div>
-          <div className="mb-4">
-            <label htmlFor="email" className="block text-gray-700">
-              Email:
-            </label>
-            <input
-              id="email"
-              type="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              className="mt-1 block w-full px-4 py-2 rounded-md border-gray-300 focus:outline-none focus:ring-blue-500 focus:border-blue-500"
-            />
-          </div>
-          <div className="flex justify-between">
-            <button
-              type="submit"
-              className="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none focus:bg-blue-600"
-            >
-              Save
-            </button>
-            <button
-              type="button"
-              onClick={handleFormCancel}
-              className="px-4 py-2 bg-gray-300 text-gray-700 rounded-md hover:bg-gray-400 focus:outline-none focus:bg-gray-400"
-            >
-              Cancel
-            </button>
-          </div>
-        </form>
+      <div className="fixed inset-0 flex items-center justify-center bg-gray-900 bg-opacity-75">
+        <div
+          className="bg-white max-w-md mx-auto rounded shadow-lg"
+          style={{ width: "30rem", height: "20rem" }}
+        >
+          <form onSubmit={handleSubmit} className="p-6">
+            <div className="mb-[3rem]">
+              <label htmlFor="name" className="block text-gray-700">
+                Name:
+              </label>
+              <input
+                id="name"
+                type="text"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                className="mt-1 block w-full px-4 py-2 rounded-md border-gray-300 focus:outline-none focus:ring-blue-500 focus:border-blue-500"
+              />
+            </div>
+            <div className="mb-4">
+              <label htmlFor="email" className="block text-gray-700">
+                Email:
+              </label>
+              <input
+                id="email"
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                className="mt-1 block w-full px-4 py-2 rounded-md border-gray-300 focus:outline-none focus:ring-blue-500 focus:border-blue-500"
+              />
+            </div>
+            <div className="flex justify-between">
+              <button
+                type="button"
+                onClick={handleFormCancel}
+                className="px-4 py-2 bg-gray-300 text-gray-700 rounded-md hover:bg-gray-400 focus:outline-none focus:bg-gray-400"
+              >
+                Cancel
+              </button>
+              <button
+                type="submit"
+                className="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none focus:bg-blue-600"
+              >
+                Update
+              </button>
+            </div>
+          </form>
+        </div>
       </div>,
-      document.getElementById("edit-portal") // Add a div with this ID in your root HTML file
+      document.getElementById("edit-portal")
     );
   }
 
-  return <button onClick={handleEditClick}>Edit</button>;
+  return (
+    <button
+      onClick={handleEditClick}
+      className="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none focus:bg-blue-600"
+    >
+      Edit
+    </button>
+  );
 };
 
 export default EditButton;
